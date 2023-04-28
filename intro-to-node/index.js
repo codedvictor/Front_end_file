68bd2829a366560ad6323966e4f7e86aa37484fe
codedvictor <victorkoladeige@gmail.com>
2023-04-28 13:05:10 +0100

    Node,js framework

---
+++ intro-to-node/index.js
@@ -0,0 +1,17 @@
+//jshint esversion:6
+
+// const fs = require("fs");
+
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+// const superheroes = require('superheroes');
+
+var superheroes = require('superheroes');
+const supervillains = require('supervillains');
+var mysh =  superheroes.random();
+ var mysp = supervillains.random();
+console.log(mysh);
+console.log(superheroes.all);
+console.log(mysp);
+console.log(supervillains.all);
+
